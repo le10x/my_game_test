e25bba887671cadb0ef710f3e29a24eff51f257a
le10x <leanderdrb@gmail.com>
2026-08-14 13:09:16 -0400

    Clear level1 map and walls configuration
    Removed level map and walls from level1.js.

--- levels/level1.js
+++ levels/level1.js
@@ -3,16 +3,7 @@ if (!window.NIVELES) window.NIVELES = [];
 
 window.NIVELES.push({
     map: [
-        ["P", ".", ".", "#", "."],
-        [".", "X", ".", ".", "."],
-        [".", ".", ".", "X", "."],
-        [".", "#", ".", ".", "."],
-        [".", ".", ".", ".", "G"]
+        ["P"]
     ],
-    walls: [
-        { x: 0, y: 0, type: 'D' },
-        { x: 2, y: 1, type: 'R' },
-        { x: 2, y: 3, type: 'U' },
-        { x: 0, y: 1, type: 'U' }
-    ]
+    walls: []
 });
