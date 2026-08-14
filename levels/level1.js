4c23d6b647a201b1e089757114108ae3194debb9
le10x <leanderdrb@gmail.com>
2026-08-14 01:17:56 -0400

    Update wall positions in level1.js

--- levels/level1.js
+++ levels/level1.js
@@ -11,8 +11,8 @@ const NIVELES = [
         ],
         // Solo un par de paredes estratégicas para probar la colisión
         walls: [
-            { x: 2, y: 0, type: 'R' }, // Pared vertical a la derecha de (2,0)
-            { x: 0, y: 3, type: 'D' }  // Pared horizontal abajo de (0,3)
+            { x: 5, y: 5, type: 'R' }, // Pared vertical a la derecha de (2,0)
+            { x: 5, y: 5, type: 'D' }  // Pared horizontal abajo de (0,3)
         ]
     }
 ];
