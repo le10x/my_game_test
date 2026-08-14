22702aef483bb27a41f4a7f88528a81c0e13d94c
le10x <leanderdrb@gmail.com>
2026-08-14 11:51:33 -0400

    Fix formatting and add missing wall definition

--- levels/level2.js
+++ levels/level2.js
@@ -7,14 +7,16 @@ const NIVELES = [
             [".", "X", ".", ".", "."],
             [".", ".", ".", "X", "."],
             [".", "#", ".", ".", "."],
-            [".", ".", ".", ".", "G"]
+            [".", ".", ".", ".", "G"],
+            ["X", "#", ".", "#", "X"]
         ],
         // Paredes delgadas estratégicas
         // L: Izquierda, R: Derecha, U: Arriba, D: Abajo
         walls: [
             { x: 0, y: 0, type: 'D' }, // Pared abajo de la salida (obliga a ir a la derecha al inicio)
             { x: 2, y: 1, type: 'R' }, // Pared a la derecha de (2,1)
-            { x: 2, y: 3, type: 'U' }  // Pared arriba de (2,3)
+            { x: 2, y: 3, type: 'U' },  // Pared arriba de (2,3)
+            { x: 0, y: 1, type: 'U' }
         ]
     }
 ];
