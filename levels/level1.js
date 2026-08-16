8527cdc20c545f1231a9f5544dff2477e44af88c
le10x <leanderdrb@gmail.com>
2026-08-16 15:37:55 -0400

    Update upper wall coordinates in level1.js

--- levels/level1.js
+++ levels/level1.js
@@ -12,7 +12,7 @@ const levelData = {
         { x: 3, y: 2 }
     ],
     walls: {
-        uWalls: [{ x: 0, y: 1 }, { x: 2, y: 4 }],
+        uWalls: [{ x: 0, y: 2 }, { x: 2, y: 4 }],
         dWalls: [],
         lWalls: [],
         rWalls: [{ x: 2, y: 1 }]
