bf6f9c9fa362ac4c49224dd4c2ea46329aef1cf1
le10x <leanderdrb@gmail.com>
2026-08-16 17:48:51 -0400

    Modify level1 data structure for new layout
    Updated level configuration with new player start and check positions, and cleared solidBlocks and over arrays.

--- levels/level1.js
+++ levels/level1.js
@@ -1,20 +1,14 @@
 const levelData = {
     cols: 7,
     rows: 7,
-    playerStart: { x: 0, y: 1 },
-    check: { x: 4, y: 5 },
-    solidBlocks: [
-        { x: 4, y: 0 },
-        { x: 1, y: 4 }
-    ],
-    over: [
-        { x: 1, y: 1 },
-        { x: 3, y: 2 }
-    ],
+    playerStart: { x: 0, y: 0 },
+    check: { x: 6, y: 6 },
+    solidBlocks: [],
+    over: [],
     walls: {
-        uWalls: [{ x: 0, y: 2 }, { x: 2, y: 4 }],
+        uWalls: [],
         dWalls: [],
         lWalls: [],
-        rWalls: [{ x: 2, y: 1 }]
+        rWalls: []
     }
 };
