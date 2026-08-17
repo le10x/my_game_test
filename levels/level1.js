b01d12be637f418031bf8a10f944863e633e0966
le10x <leanderdrb@gmail.com>
2026-08-17 18:47:59 -0400

    Update level1.js configuration for new level design

--- levels/level1.js
+++ levels/level1.js
@@ -1,14 +1,19 @@
 const levelData = {
-    cols: 7,
-    rows: 7,
-    playerStart: { x: 0, y: 0 },
-    check: { x: 6, y: 6 },
-    solidBlocks: [],
-    over: [],
-    walls: {
-        uWalls: [],
-        dWalls: [],
-        lWalls: [],
-        rWalls: []
-    }
+    cols: 5,
+    rows: 5,
+    playerStart: { x: 0, y: 1 },
+    check: { x: 4, y: 4 },
+    solidBlocks: [
+        { x: 3, y: 0 },
+        { x: 1, y: 3 }
+    ],
+    overs: [
+        { x: 1, y: 1 },
+        { x: 3, y: 2 }
+    ],
+    walls: [
+        { x: 0, y: 1, dir: 'U' },
+        { x: 2, y: 1, dir: 'R' },
+        { x: 2, y: 3, dir: 'D' }
+    ]
 };
