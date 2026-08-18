d9ee73d2208f8c9c76c89c0af9aa6007256c37c3
le10x <leanderdrb@gmail.com>
2026-08-18 19:16:27 -0400

    Modify level1 dimensions and player start position
    Reduced level dimensions and adjusted player start position.

--- levels/level1.js
+++ levels/level1.js
@@ -1,19 +1,20 @@
 const levelData = {
-    cols: 5,
-    rows: 5,
-    playerStart: { x: 0, y: 1 },
+    cols: 4,
+    rows: 4,
+    playerStart: { x: 0, y: 0 },
     check: { x: 4, y: 4 },
     solidBlocks: [
-        { x: 3, y: 0 },
-        { x: 1, y: 3 }
+        { x: 0, y: 1 },
+        { x: 0, y: 2 }
     ],
     overs: [
         { x: 1, y: 1 },
-        { x: 3, y: 2 }
+        { x: 1, y: 2 }
     ],
     walls: [
-        { x: 0, y: 1, dir: 'U' },
-        { x: 2, y: 1, dir: 'R' },
-        { x: 2, y: 3, dir: 'D' }
+        { x: 2, y: 0, dir: 'U' },
+        { x: 2, y: 0, dir: 'D' },
+        { x: 2, y: 0, dir: 'L' },
+        { x: 2, y: 0, dir: 'R' }
     ]
 };
