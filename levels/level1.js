ea04f5da5f2db8e10ddd96a4232f38b7786a3b26
le10x <leanderdrb@gmail.com>
2026-08-18 19:32:23 -0400

    Adjust level configuration in level1.js
    Updated the check position and modified solidBlocks, overs, and walls arrays.

--- levels/level1.js
+++ levels/level1.js
@@ -2,19 +2,19 @@ const levelData = {
     cols: 4,
     rows: 4,
     playerStart: { x: 0, y: 0 },
-    check: { x: 4, y: 4 },
+    check: { x: 3, y: 3 },
     solidBlocks: [
-        { x: 0, y: 1 },
-        { x: 0, y: 2 }
+        { x: 1, y: 0 },
+        { x: 1, y: 1 }
     ],
     overs: [
-        { x: 1, y: 1 },
-        { x: 1, y: 2 }
+        { x: 2, y: 0 },
+        { x: 4, y: 0 }
     ],
     walls: [
-        { x: 2, y: 0, dir: 'U' },
-        { x: 2, y: 0, dir: 'D' },
-        { x: 2, y: 0, dir: 'L' },
-        { x: 2, y: 0, dir: 'R' }
+        { x: 2, y: 1, dir: 'U' },
+        { x: 2, y: 1, dir: 'D' },
+        { x: 2, y: 1, dir: 'L' },
+        { x: 2, y: 1, dir: 'R' }
     ]
 };
